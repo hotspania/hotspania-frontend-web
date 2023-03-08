c5ec17041b43d53f100b3f961c628bbc0b0d5606
hotspania <hotspania@gmail.com>
2023-03-08 18:37:06 +0100

    New changes in URL APIs

--- src/app/services/global.ts
+++ src/app/services/global.ts
@@ -1,6 +1,6 @@
 export const Global ={
     url1:'http://localhost:3500/web/',
-    url:'http://185.166.214.67:3500/web/',
-    urlimages:"http://185.166.214.67:3500/api/", 
-    urlimages1:'http://localhost:3500/api/',
+    url:'https://api.hotspania.es/web/',
+    urlimages:"https://api.hotspania.es/api/", 
+    urlimages1:'https://localhost:3500/api/',
  }
