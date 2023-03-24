66861976c7b88e5a68d1303aca92cdaf07ecd9d8
Hotspania <hotspania@gmail.com>
2023-03-25 00:43:20 +0100

    Register web done

--- src/app/pages/register/register.component.ts
+++ src/app/pages/register/register.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { RegisterService } from '../../services/register.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-register',
@@ -7,25 +9,50 @@ import { FormControl, FormGroup, Validators } from '@angular/forms';
   styleUrls: ['./register.component.scss'],
 })
 export class RegisterComponent implements OnInit {
+  modal_loading: boolean = false;
   'scripts': ['./register.js'];
-  protected form: FormGroup;
+  public fakeDataForm: FormGroup;
+  public filesDataForm: FormGroup;
+  public realDataForm: FormGroup;
+  private id: string = "";
   private files: any[] = [];
+  public archivo: File;
+  public images:any=[];
+  public ImagenTemp: string;
 
-  constructor() {
-    this.form = new FormGroup({
-      name: new FormControl(null, Validators.required),
-      age: new FormControl(null, Validators.required),
-      weight: new FormControl(null, Validators.required),
-      height: new FormControl(null, Validators.required),
-      measures1: new FormControl(null, Validators.required),
-      measures2: new FormControl(null, Validators.required),
-      measures3: new FormControl(null, Validators.required),
-      //horario: new FormControl(null, Validators.required),
-      zone: new FormControl(null, Validators.required),
-      smoke: new FormControl(null, Validators.required),
-      phone: new FormControl(null, Validators.required),
+  constructor(private _registerService: RegisterService, private router: Router) {
+    this.realDataForm = new FormGroup({
+      nombre: new FormControl(null, Validators.required),
       email: new FormControl(null, Validators.required),
       nif: new FormControl(null, Validators.required),
+      dni: new FormControl(null, Validators.required),
+      telefono: new FormControl(null, Validators.required),
+    });
+
+    this.fakeDataForm = new FormGroup({
+      id: new FormControl(null, Validators.required),
+      username: new FormControl(null, Validators.required),
+      edad: new FormControl(null),
+      //whatsapp: new FormControl(null),
+      telefono: new FormControl(null),
+      //atencion: new FormControl(this.atencion),
+      //tags: new FormControl(null),
+      fumadora: new FormControl('Si', Validators.required),
+      zonas: new FormControl(null, Validators.required),
+      //genero: new FormControl(null, Validators.required),
+      //servicios: new FormControl('Normales', Validators.required),
+      estatura: new FormControl(null),
+      peso: new FormControl(null),
+      busto: new FormControl(null),
+      cintura: new FormControl(null),
+      cadera: new FormControl(null),
+      inicio: new FormControl(null, Validators.required),
+      fin: new FormControl(null, Validators.required),
+      horario_inicio: new FormControl(null, Validators.required),
+      horario_fin: new FormControl(null, Validators.required)
+    });
+
+    this.filesDataForm = new FormGroup({
       files: new FormControl(null, Validators.required),
     });
   }
@@ -34,130 +61,198 @@ export class RegisterComponent implements OnInit {
     this.getScript();
   }
 
-  protected onChange(event: any): void {
+  seleccionImagen(file: File) {
+
+    if (!file) {
+      this.archivo = null;
+      return;
+    }   
+    if (file.type.indexOf('image') < 0) {
+      return;
+    }
+    if (file.size > 10000000) {
+      return;
+    }
+    let reader = new FileReader();
+    let UrlImageTemp = reader.readAsDataURL(file);
+    reader.onloadend = () =>this.ImagenTemp = reader.result.toString();   
+    this.archivo=file;
+  }
+
+  public onSubmit1() {
+    this._registerService.registerRealData(this.realDataForm.value).subscribe((resp:any)=>{
+      console.log('rr', resp);
+      if(resp.ok){
+
+        console.log('dw', resp.id);
+        this.id=resp.id;
+        console.log('real data ok');
+          this._registerService.subirArchivo(this.archivo,'dni',this.id).then((updata:any)=>{
+            this.modal_loading=false;
+            console.log('foto dni ok');
+          }).catch((error)=>{
+            console.error('error foto dni');
+          })
+      }
+    });
+
+  }
+
+  public onSubmit2() {
+    if(this.files.length==1){
+      this._registerService.subirArchivo(this.files[0], 'original', this.id).then((resp:any)=>{
+        if (resp.ok) {
+          console.log('files ok');
+        }
+      });
+    }else{
+      this._registerService.registerFiles(this.files, 'original', this.id).then((resp:any)=>{
+        if (resp.ok) {
+          console.log('files ok');
+        }
+      });
+    }
+
+  }
+
+  public onSubmit3() {
+    this.fakeDataForm.value.id = this.id;
+    this._registerService.registerFakeData(this.fakeDataForm.value).subscribe((resp: any) => {
+      if (resp.ok) {
+        console.log('fake data ok');
+      }
+    });
+
+    /*setTimeout(() => {
+       this.modal_loading=false;
+       this.router.navigate(['/']);
+     }, 2000);*/
+  }
+  public onChange(event: any): void {
     const target = event.target;
 
     const value = target.value;
     const name = target.name;
     const id = target.id;
 
-    switch(name){
+    switch (name) {
       case 'name':
         isNaN(value) && !this.checkUndefinedOrNull(value) ? this.validate(id, true) : this.validate(id, false);
-      break;
+        break;
 
       case 'age':
-        !isNaN(value) && value>=18 && !this.checkUndefinedOrNull(value) ? this.validate(id, true) : this.validate(id, false);
-      break;
+        !isNaN(value) && value >= 18 && !this.checkUndefinedOrNull(value) ? this.validate(id, true) : this.validate(id, false);
+        break;
+
+      case 'height':
+        !isNaN(value) && !this.checkUndefinedOrNull(value) ? this.validate(id, true) : this.validate(id, false);
+        break;
 
       case 'weight':
         !isNaN(value) && !this.checkUndefinedOrNull(value) ? this.validate(id, true) : this.validate(id, false);
-      break;
+        break;
 
       case 'weight':
         !isNaN(value) && !this.checkUndefinedOrNull(value) ? this.validate(id, true) : this.validate(id, false);
-      break;
-
-      case 'weight':
-        !isNaN(value) && !this.checkUndefinedOrNull(value) ? this.validate(id, true) : this.validate(id, false);
-      break;
+        break;
 
       case 'measures1':
         !isNaN(value) && !this.checkUndefinedOrNull(value) ? this.validate(id, true) : this.validate(id, false);
-      break;
+        break;
 
       case 'measures2':
         !isNaN(value) && !this.checkUndefinedOrNull(value) ? this.validate(id, true) : this.validate(id, false);
-      break;
+        break;
 
       case 'measures3':
         !isNaN(value) && !this.checkUndefinedOrNull(value) ? this.validate(id, true) : this.validate(id, false);
-      break;
+        break;
+
+      case 'start_day':
+        isNaN(value) && !this.checkUndefinedOrNull(value) ? this.validate(id, true) : this.validate(id, false);
+        break;
+
+      case 'end_day':
+        isNaN(value) && !this.checkUndefinedOrNull(value) ? this.validate(id, true) : this.validate(id, false);
+        break;
+
+      case 'start_time':
+        value.match(/^([0-9]|0[0-9]|1[0-9]|2[0-3]):[0-5][0-9]$/) && !this.checkUndefinedOrNull(value) ? this.validate(id, true) : this.validate(id, false);
+        break;
+
+      case 'end_time':
+        value.match(/^([0-9]|0[0-9]|1[0-9]|2[0-3]):[0-5][0-9]$/) && !this.checkUndefinedOrNull(value) ? this.validate(id, true) : this.validate(id, false);
+        break;
 
       case 'zone':
         isNaN(value) && !this.checkUndefinedOrNull(value) ? this.validate(id, true) : this.validate(id, false);
-      break;
+        break;
 
       case 'smoke':
         isNaN(value) && !this.checkUndefinedOrNull(value) ? this.validate(id, true) : this.validate(id, false);
-      break;
+        break;
 
       case 'phone':
         !isNaN(value) && value.length === 9 && !this.checkUndefinedOrNull(value) ? this.validate(id, true) : this.validate(id, false);
-      break;
+        break;
+
+      case 'real_name':
+        isNaN(value) && !this.checkUndefinedOrNull(value) ? this.validate(id, true) : this.validate(id, false);
+        break;
 
       case 'email':
-        value.math(/^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/)
-         && !this.checkUndefinedOrNull(value) ? this.validate(id, true) : this.validate(id, false);
-      break;
+        value.match(/^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/)
+          && !this.checkUndefinedOrNull(value) ? this.validate(id, true) : this.validate(id, false);
+        break;
 
       case 'nif':
         const allowedExtensions = ['jpeg', 'jpg', 'png', 'webp', 'gif', 'bmp'];
         const fileExtension = value.split('.').pop().toLowerCase();
         allowedExtensions.includes(fileExtension) && !this.checkUndefinedOrNull(value) ? this.validate(id, true) : this.validate(id, false);
-      break;
+        break;
+
+      case 'nif_pass':
+        value.match(/([a-z]|[A-Z]|[0-9])[0-9]{7}([a-z]|[A-Z]|[0-9])/) && !this.checkUndefinedOrNull(value) ? this.validate(id, true) : this.validate(id, false);
+        break;
 
       case 'files':
         const allowedExtensions2 = ['jpeg', 'jpg', 'png', 'webp', 'gif', 'bmp', 'mp4', 'avi', 'mpg', 'mov', 'wmv', 'flv', '3gp'];
         let totalErrors = 0;
         this.files.map((val) => {
           const fileExtension = val.name.split('.').pop().toLowerCase();
-          if(!allowedExtensions2.includes(fileExtension)){
+          if (!allowedExtensions2.includes(fileExtension)) {
             totalErrors++;
           }
         });
         totalErrors === 0 && !this.checkUndefinedOrNull(value) ? this.validate(id, true) : this.validate(id, false);
-      break;
+        break;
+    }
+    this.validateSection();
+  }
+
+  private validateSection() {
+    const totalSection = 3;
+    for (let i = 1; i <= totalSection; i++) {
+      let errors = 0;
+      $(`.step${i}input`).each(function (index) {
+        if (!$(this).hasClass('correct')) {
+          errors++;
+        }
+      });
+
+      if (errors === 0) {
+        $(`.btnstep${i}`).removeClass('disabled');
+        $(`.btnstep${i}`).prop('disabled', false);
+      } else {
+        $(`.btnstep${i}`).addClass('disabled');
+        $(`.btnstep${i}`).prop('disabled', true);
+      }
     }
   }
 
-  protected validateSection(){
-    /*const totalSection = 3;
-    for(let i = 1; i <= totalSection; i++){
-      let totalInputs = 0;
-      let errors = 0;
-      let success = 0;
-      $(`#step-${i} input`).each(function( index ) {
-        totalInputs++;
-          if($(this).val() && $(this).attr('type') !== 'radio'){
-            if(!$(this).hasClass('correct')){
-              errors++;
-            }
-          }
-      });
-
-      console.log($(`.step-${i}`));
-
-      $(`#step-${i} select`).each(function( index ) {
-        totalInputs++;
-        if($(this).val()){
-          if(!$(this).hasClass('correct')){
-            errors++;
-          }
-        }
-      });
-      
-      console.log('e', errors);
-
-      console.log('ewgewg', $(`.step-${i}`));
-      if(errors > 0 || success < totalInputs){
-        $(`.step-${i}-circle`).css('background', 'red');
-        $(`.step-${i}`).removeClass('form-stepper-completed');
-        $(`.step-${i}`).removeClass('form-stepper-active');
-        $(`.step-${i}`).addClass('form-stepper-unfinished');
-      }
-
-      if(success === 0){
-        $(`.step-${i}`).removeClass('form-stepper-active');
-        $(`.step-${i}`).removeClass('form-stepper-unfinished');
-        $(`.step-${i}`).addClass('form-stepper-completed');
-      }
-    } */
-  }
-
-  private validate(id: any, bool: boolean):void {
+  private validate(id: any, bool: boolean): void {
     const selectorID = $(`#${id}`);
-    if(bool){
+    if (bool) {
       selectorID.addClass('correct');
       selectorID.removeClass('error');
     } else {
@@ -166,18 +261,25 @@ export class RegisterComponent implements OnInit {
     }
   }
 
-  protected onFileChange(event:any) {
+  public onFileChange(f: any) {
     this.files = [];
-    const target = event.target;
-    const files =  Array.from(target.files);
-    files.map((targ) => {
+    const files = Array.from(f);
+    files.map((targ:any) => {
       this.files.push(targ);
+
+      let reader = new FileReader();
+      let UrlImageTemp = reader.readAsDataURL(targ);
+      reader.onloadend = () => this.images.push({
+        nombre: targ.name,
+        imagen: reader.result.toString()
+      });
+
     });
 
-}
+  }
 
   private checkUndefinedOrNull(value: any): boolean {
-    if(value === null || value === undefined || value === ""){
+    if (value === null || value === undefined || value === "") {
       return true;
     } else {
       return false;
--- src/app/services/global.ts
+++ src/app/services/global.ts
@@ -2,5 +2,5 @@ export const Global ={
     url1:'http://localhost:3500/web/',
     url:'https://api.hotspania.es/web/',
     urlimages:"https://api.hotspania.es/api/", 
-    urlimages1:'https://localhost:3500/api/',
+    urlimages1:'http://localhost:3500/api/',
  }
